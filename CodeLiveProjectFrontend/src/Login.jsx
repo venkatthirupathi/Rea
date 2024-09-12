6ad1ef2c470fda9ee4f154d926b28fb8988b4617
venkatthirupathi <thirupathivenkat2005@gmail.com>
2024-09-12 23:29:09 +0530

    first commit

--- CodeLiveProjectFrontend/src/Login.jsx
+++ CodeLiveProjectFrontend/src/Login.jsx
@@ -75,8 +75,12 @@ const Login = () => {
                     <p className='preview-content'>Experience the future of collaborative coding with our innovative and powerful Collaborative Code Editor. Empower your team to work together seamlessly, boost productivity, and bring your ideas to life like never before. Start coding together, and let the possibilities unfold.</p>               
                 </div>
             </div>
+            
 
-            <p>Currently SignUp, Lock, And Chat Features are partially implemented ...</p>
+
+
+
+            <p>Currently SignUp, Chat Features are partially implemented ...</p>
 
             <section className='mb-14 flex'>
             <div className=''>
@@ -120,38 +124,38 @@ const Login = () => {
             </section>
 
             <section className='todos'>
+            
                 <h2> To-Do  <span className='highlite'>S </span> </h2>
                 <p>We welcome collaboration and invite you to contribute your ideas and expertise to enhance our project. Your valuable input is highly appreciated as we work together to create something exceptional. Feel free to join us and make a meaningful impact with your contributions.</p>
                 <ul>
-                   <a href="https://github.com/Keerthivardhan1/CodeLiveProject/issues/1"><li>Enable Simultaneous Coding </li></a>
-                    <a href=""><li>Efficient Database Implementation for Storing Individual Team's Code <span className='highlite'>- Done</span> </li></a>
-                   <a href="https://github.com/Keerthivardhan1/CodeLiveProject/issues/2"><li>Enhanced Code Editor: Customizable Theme Selection and UI</li></a> 
-                   <a href="https://github.com/Keerthivardhan1/CodeLiveProject/issues/3"><li>Advanced Speech Recognition Module: Hands-free Coding Experience</li></a> 
-                   <a href="https://github.com/Keerthivardhan1/CodeLiveProject/issues/4">
+                   <a href="https://github.com/venkatthirupathi/Real-Time-Code-Editor/issues/1"><li>Enable Simultaneous Coding </li></a>
+                    <a href=""><li>Efficient Database Implementation for Storing Individual Team's Code <span className='highlite'></span> </li></a>
+                   <a href="https://github.com/venkatthirupathi/Real-Time-Code-Editor/issues/2"><li>Enhanced Code Editor: Customizable Theme Selection and UI</li></a> 
+                   <a href="https://github.com/venkatthirupathi/Real-Time-Code-Editor/issues/3"><li>Advanced Speech Recognition Module: Hands-free Coding Experience</li></a> 
+                   <a href="https://github.com/venkatthirupathi/Real-Time-Code-Editor/issues/4">
                     <li>Implement Encreption and Decreption Algorithem At Server and Client Side</li>
                    </a>
-                   <a href="https://github.com/Keerthivardhan1/CodeLiveProject"> <li>Contribute To Project</li></a>
+                   <a href="https://github.com/venkatthirupathi/Real-Time-Code-Editor/"> <li>Contribute To Project</li></a>
                 </ul>
-
             </section>
 
 
             <footer>
                 <ul>
-                    <a href="https://github.com/Keerthivardhan1">
+                    <a href="https://github.com/venkatthirupathi">
                         <img src={git} alt=""  className="social-icon"/>
                     </a>
-                    <a href="https://www.linkedin.com/in/keerthi-vardhan-tekulapelli-7064a6245/">
+                    <a href="https://www.linkedin.com/in/thirupathivenkat/">
                         <img src={linkedin} alt="" className="social-icon"/>
                     </a>
-                    <a href="https://hashnode.com/@keerthivardhan">
+                    <a href="https://hashnode.com/@venkat123">
                         <img src={hashnode} alt="" className="social-icon"/>
                     </a>
-                    <a href="https://twitter.com/vardhan132003">
+                    <a href="https://x.com/T_VENKAT_2005">
                         <img src={twitter} alt="" className="social-icon"/>
                     </a>
                 </ul>
-                <center className='highlite'><h3>KEERTHI VARDHAN <br /></h3>keerthivardhantekulapelli@gmial.com</center>
+                <center className='highlite'><h3>VENKAT THIRUPATHI<br /></h3>thirupathivenkat2005@gmail.com</center>
                 
             </footer>       
         </motion.div>
